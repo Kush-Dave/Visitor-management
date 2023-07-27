41d1a8eba1518addcfc39806b4f4949033af0010
Kush-Dave <davekush1996@gmail.com>
2023-07-27 17:38:37 +0530

    make a visitor's enquiry form with database

--- visitor/enquire/models.py
+++ visitor/enquire/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date, datetime
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class detail(models.Model):
+  name = models.CharField(max_length=50, default=False)
+  number = models.IntegerField(default=False)
+  email = models.EmailField(max_length=50, default=False)
+  company_name = models.CharField(max_length=50,default=False)
+  # products = 
+  date = models.DateField(_("Date"), default=date.today)
--- visitor/enquire/views.py
+++ visitor/enquire/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import detail
+from datetime import date
 # Create your views here.
+
+def index(request):
+  return render(request, 'index.html')
+
+def home(request):
+  if request.method == 'POST':
+    name = request.POST['name']
+    number = request.POST['number']
+    email = request.POST['email']
+    company_name = request.POST['company_name']
+    
+    eve = detail.objects.create(name=name,number=number,email=email,company_name=company_name,date=date)
+    eve.save()
+  return render(request, 'home.html')
